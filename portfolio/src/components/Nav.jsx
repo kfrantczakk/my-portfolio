dbbe04c97f4516c1e89c624c49ca5e6b966dddcc
Kacper Frantczak <kfrantczakk@gmail.com>
2025-03-03 09:31:01 +0100

    1

---
+++ portfolio/src/components/Nav.jsx
@@ -0,0 +1,10 @@
+const nav = () => {
+  return <nav className="mb-20 flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+        
+    </div>
+  </nav>
+  
+}
+
+export default nav
