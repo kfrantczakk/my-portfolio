fe7a1fe22df246157ad844f64cb787b9b092ccb8
Kacper Frantczak <kfrantczakk@gmail.com>
2025-03-04 09:02:46 +0100

    1

---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className="border-b border-slate-950  pb-4  lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1>Kim jestem?</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,10 +1,18 @@
-const nav = () => {
-  return <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
-        
-    </div>
-  </nav>
-  
-}
+import { FaGithub, FaLink } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
-export default nav
+const Nav = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="text-2xl font-bold">Kacper Frantczak</h1>
+      </div>
+      <div className="m-8 flex item-center justify-center gap-4 text-2xl">
+        <FaGithub />
+        <FaLinkedin />
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
